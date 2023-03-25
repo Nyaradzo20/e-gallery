fc6890da4f7632db50f646897e7f9e73a1e20d6c
katman-Generation <tkatema5@gmail.com>
2023-03-25 07:38:42 +0200

    adjusting the pages to respond well

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ def  homePage():
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'firstname' in request.form and 'lastname' in request.form and 'username' in request.form and 'email' in request.form and 'password' in request.form:
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        username = request.form['username']
+    if request.method == 'POST' and 'fullname' in request.form and 'dateOfBirth' in request.form  and 'email' in request.form and 'password' in request.form and 'repassword' in request.form:
+        fullname = request.form['fullname']
+        dateOfBirth = request.form['dateOfBirth']
         email = request.form['email']
         password = request.form['password']
+        repassword = request.form['repassword']
         
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM account WHERE email = %s', (email, ))
@@ -44,14 +44,18 @@ def register():
             msg = 'account already exist !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
+        elif not re.match(r'[A-Za-z0-9]+', fullname):
             msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email or not firstname or not lastname:
+        elif not fullname or not password or not email or not dateOfBirth or not repassword:
             msg = 'please fill out the form'
         else:
-            cursor.execute('INSERT INTO account VALUES (NULL, % s, % s, % s, % s, % s)', (firstname, lastname, username, email, password, ))
-            mysql.connection.commit()
-            msg =  'you have successfully registered'
+            if password == repassword:
+                password= password
+                cursor.execute('INSERT INTO account VALUES (NULL, % s, % s, % s, % s)', (fullname, dateOfBirth, email, password))
+                mysql.connection.commit()
+                msg =  'you have successfully registered'
+            else:
+                msg = 'The passwords does not match'
     elif request.method == 'POST':
         msg = 'please fill out the form!'
     return render_template('register.html', msg = msg)
